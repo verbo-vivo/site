0a45cce2eb25cf811564c29cd0616ee4b8d96b93
Moises P. Sena <moisespsena@gmail.com>
2022-02-03 05:36:07 -0300

    Create 03 - Aguas Calmas e Refrigério.d

---
+++ sl/23/03 - Aguas Calmas e Refrigério.d
@@ -0,0 +1,13 @@
+# SALMO 23 - Águas Calmas e Refrigério
+
+DEVOCIONAL CBPAZ PAULA CÂNDIDO - 28/01/2022 
+
+> O Senhor é o meu pastor; nada me faltará. Ele me faz repousar em pastos verdejantes. *Leva-me para junto das águas de descanso;
+refrigera-me a alma*. Guia-me pelas veredas da justiça por amor do seu nome. Ainda que eu ande pelo vale da sombra da morte, não temerei mal nenhum,  porque tu estás comigo; o teu bordão e o teu cajado me consolam. Preparas-me uma mesa na presença dos meus adversários, unges a minha cabeça com óleo; o meu cálice transborda. Bondade e misericórdia certamente me seguirão todos os dias da minha vida; e habitarei na Casa do Senhor para todo o sempre. (Salmo 23)
+Neste salmo, o escritor, Davi, um pastor de ovelhas, faz uma reflexão sobre o cuidado de Deus para consigo como se as ovelhas de seu pai estivessem descrevendo sua diligência, responsabilidade e cuidado para com elas.
+
+
+Tenha um ótimo dia!!
+
+----
+Moisés P. Sena.
